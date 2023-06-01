5daca5b65444a0aa3f72c847c3d585bacfcb6c1c
atsupi <atsupi@gmail.com>
2023-06-01 22:15:37 +0900

    Added function to get secrets.

--- Utils.js
+++ Utils.js
@@ -1,19 +1,62 @@
-import * as dotenv from 'dotenv';
 import fs from 'fs'
 import path from 'path';
 import AWS from 'aws-sdk';
 import { fileURLToPath } from 'url';
 import { execSync } from 'child_process';
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+const secret_name1 = "ACCESS_KEY";
+const secret_name2 = "SECRET_ACCESS_KEY";
+const secret_name3 = "S3_BUCKET_NAME";
+
+const client = new SecretsManagerClient({
+    region: "ap-northeast-1",
+});
+
+let response1;
+let response2;
+let response3;
+
+try {
+    response1 = await client.send(
+        new GetSecretValueCommand({
+            SecretId: secret_name1,
+            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        })
+    );
+    response2 = await client.send(
+        new GetSecretValueCommand({
+            SecretId: secret_name2,
+            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        })
+    );
+    response3 = await client.send(
+        new GetSecretValueCommand({
+            SecretId: secret_name3,
+            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        })
+    );
+} catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+}
+
+const secret1 = response1.SecretString;
+const secret2 = response2.SecretString;
+const secret3 = response3.SecretString;
+
 
 AWS.config.update({
     credentials: new AWS.Credentials(
-        process.env.ACCESS_KEY,
-        process.env.SECRET_ACCESS_KEY
+        secret1, // access key
+        secret2, // secret access key
     ),
     region: "ap-northeast-1"
 });
@@ -22,10 +65,12 @@ const s3 = new AWS.S3();
 
 export async function s3FileDownload(key, localfile) {
     console.log("s3FileDownload", key, path.join(__dirname, localfile));
-    console.log(process.env.S3_BUCKET_NAME);
+    console.log(secret1);
+    console.log(secret2);
+    console.log(secret3);
     try {
         const uploaded_data = await s3.getObject({
-            Bucket: process.env.S3_BUCKET_NAME,
+            Bucket: secret3,
             Key: key
         }).promise()
             .catch((err) => console.log(err));
@@ -41,7 +86,7 @@ export async function s3FileDownload(key, localfile) {
 export async function s3FileUpload(key, localfile) {
     try {
         await s3.putObject({
-            Bucket: process.env.S3_BUCKET_NAME,
+            Bucket: secret3,
             Key: key,
             Body: fs.createReadStream(path.join(__dirname, localfile)),
             ContentType: "video/mp4"
